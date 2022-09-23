50f792d22f817edd56ca06245893d91f987a87f8
hshine1226 <hshine1226@gmail.com>
2022-09-23 09:16:49 +0900

    Update printer-queue-1966

--- day_1/jhchoi/printer-queue-1966.py
+++ day_1/jhchoi/printer-queue-1966.py
@@ -1,10 +1,10 @@
 from collections import deque
 import sys
+
 def input():
     return sys.stdin.readline().rstrip()
 
 testCases = int(input())
-
 for _ in range(testCases):
     n, m = map(int, input().split())
     count = 0
@@ -12,14 +12,11 @@ for _ in range(testCases):
     queue = deque(enumerate(scores))
 
     while(1):
-        currentIdx, currentScore = queue.popleft()
-        if(len(queue) == 0):
-            print(count+1)
-            break
-        if currentScore >= max(queue, key = lambda x:x[1])[1]: 
+        if queue[0][1] >= max(queue, key = lambda x:x[1])[1]: 
             count += 1
+            currentIdx, _ = queue.popleft()
             if(currentIdx == m):
                 print(count)
                 break
         else:
-            queue.append((currentIdx, currentScore))
+            queue.rotate(1)
