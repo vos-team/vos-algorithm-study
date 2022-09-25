511aa875d91dce3872ef0685e83bdd2d972206e2
hshine1226 <hshine1226@gmail.com>
2022-09-25 10:55:16 +0900

    Add zargade-2800

---
+++ day_1/jhchoi/zagrade-2800.py
@@ -0,0 +1,26 @@
+import sys
+from itertools import combinations
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+expressions = list(map(str, input()))
+
+stack, brackets = [], []
+resultSet = set()
+# 괄호쌍 찾기
+for idx, ex in enumerate(expressions): 
+    if ex == '(':
+        stack.append(idx)
+    if ex == ')':
+        brackets.append((stack.pop(), idx))
+# 괄호쌍 제거 조합
+for i in range(len(brackets)): 
+    for combination in combinations(brackets, i + 1):
+        tmp = expressions[:]
+        for s, e in combination:
+            tmp[s] = ''
+            tmp[e] = ''
+        resultSet.add(''.join(tmp))
+for result in sorted(list(resultSet)): 
+    print(result)
