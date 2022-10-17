86a30378588e49d95fe137e238dc1c25640f3ca4
nanangqq <gingggg@gmail.com>
2022-10-17 23:30:42 +0900

    1번 정렬 -> quicksort 적용하라

--- day_4/khyoo/00_ascending.py
+++ day_4/khyoo/00_ascending.py
@@ -2,5 +2,27 @@ import sys
 
 nums = list(map(int, sys.stdin.readlines()))
 cnt = nums[0]
-for num in sorted(nums[1:]):
+
+# for num in sorted(nums[1:]):
+#     print(num)
+
+sys.setrecursionlimit(10000**2)
+
+
+def quicksort(nums):
+    if len(nums):
+        p = nums[0]
+        lower = []
+        upper = []
+
+        for num in nums[1:]:
+            if num < p:
+                lower.append(num)
+            else:
+                upper.append(num)
+        return quicksort(lower) + [p] + quicksort(upper)
+    return nums
+
+
+for num in quicksort(nums[1:]):
     print(num)
