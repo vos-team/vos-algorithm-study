4c2903a7c80ef963e5288fcba8ff249e138e9309
nanangqq <gingggg@gmail.com>
2022-10-30 18:14:05 +0900

    말숭이

--- day_6/khyoo/run.py
+++ day_6/khyoo/run.py
@@ -34,12 +34,35 @@ cc_input_2 = """6 8
 2 4
 2 3"""
 
+monkey_input = """1
+4 4
+0 0 0 0
+1 0 0 0
+0 0 1 0
+0 1 0 0"""
+
+monkey_input_2 = """2
+5 2
+0 0 1 1 0
+0 0 1 1 0"""
+
+monkey_input_3 = '''3
+4 5
+0 1 1 1
+1 1 0 1
+1 1 1 1
+1 1 1 0
+1 1 1 0'''
+
 tests = [
     ("00_search.py", search_input),
     ("00_search.py", search_input_2),
     ("00_search.py", search_input_3),
     ("01_cc.py", cc_input),
     ("01_cc.py", cc_input_2),
+    ("02_monkey.py", monkey_input),
+    ("02_monkey.py", monkey_input_2),
+    ("02_monkey.py", monkey_input_3),
 ]
 
 for fp, input_text in tests:
