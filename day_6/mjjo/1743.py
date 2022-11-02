22cbd07e5fbf280fdffc1d94c85944f400636412
jomujin <jomujin@gmail.com>
2022-11-02 22:04:15 +0900

    1743 풀이 완료

--- day_6/mjjo/1743.py
+++ day_6/mjjo/1743.py
@@ -4,58 +4,37 @@ from collections import deque
 sys.stdin = open('day_6/mjjo/1743.txt')
 input = sys.stdin.readline
 N, M, K = map(int, input().split())
-print(N, M, K)
 board = [[0]*M for _ in range(N)]
-visited = [[0] for _ in range(N)]
+visited = [[True]*M for _ in range(N)]
 trash_loc = [list(map(int, input().split())) for _ in range(K)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+max_cnt = 0
+
 for loc in trash_loc:
     x, y = loc
     board[x-1][y-1] = 1
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+def bfs(x, y):
+    cnt = 1
+    _deque = deque()
+    _deque.append([x, y])
+    visited[x][y] = False
 
-for _ in board:
-    print(_)
-print(trash_loc)
-
-def is_no_way(x, y):
-    for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if (0<=nx<N) and (0<=ny<M) and (board[nx][ny] == 1):
-            return True
-    return False
-
-def func(x, y):
-    # check = [[0 for _ in range(4)] for _ in range(4)]
-    d = deque()
-    # check[x][y] = 0
-    cnt = 0
-    d.append([x, y])
-
-    while d:
-        x, y = d.popleft()
-        if is_no_way(x, y):
-            print('noway')
-            return cnt
+    while _deque:
+        x, y = _deque.popleft()
         for i in range(4):
-            # print(check)
-            nx = x+dx[i]
-            ny = y+dy[i]
-            print(nx, ny)
-            if (0<=nx<N) and (0<=ny<M) and (board[nx][ny] == 1):
-                d.append([nx, ny])
-                # check[nx][ny] = check[x][y]+1
+            nx, ny = x+dx[i], y+dy[i]
+            if (0<=nx<N) and (0<=ny<M) and (board[nx][ny] == 1) and (visited[nx][ny]):
+                visited[nx][ny] = False
+                _deque.append([nx, ny])
                 cnt += 1
     return cnt
 
-pts = []
-for loc in trash_loc:
-    print(loc)
-    x, y = loc
-    pt = func(x-1, y-1)
-    pts.append(pt)
+for x in range(N):
+    for y in range(M):
+        if (board[x][y] == 1) and (visited[x][y]):
+            cnt = bfs(x, y)
+            max_cnt = max(max_cnt, cnt)
 
-print(pts)
-
+print(max_cnt)
