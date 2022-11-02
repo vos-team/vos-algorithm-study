84f80a2a53439dd3d1590be5bf1905a4b38a00e6
jomujin <jomujin@gmail.com>
2022-11-02 22:34:01 +0900

    1743 코드 정리

--- day_6/mjjo/1743.py
+++ day_6/mjjo/1743.py
@@ -6,30 +6,28 @@ input = sys.stdin.readline
 N, M, K = map(int, input().split())
 board = [[0]*M for _ in range(N)]
 visited = [[True]*M for _ in range(N)]
-trash_loc = [list(map(int, input().split())) for _ in range(K)]
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 max_cnt = 0
 
 def bfs(x, y):
     cnt = 1
-    _deque = deque()
-    _deque.append([x, y])
+    queue = deque([[x, y]])
     visited[x][y] = False
 
-    while _deque:
-        x, y = _deque.popleft()
+    while queue:
+        x, y = queue.popleft()
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
             if (0<=nx<N) and (0<=ny<M) and (board[nx][ny]) and (visited[nx][ny]):
                 visited[nx][ny] = False
-                _deque.append([nx, ny])
+                queue.append([nx, ny])
                 cnt += 1
     return cnt
 
-for loc in trash_loc:
-    x, y = loc
-    board[x-1][y-1] = 1
+for _ in range(K):
+    x, y = map(lambda x: int(x)-1, input().split())
+    board[x][y] = 1
 
 for x in range(N):
     for y in range(M):
