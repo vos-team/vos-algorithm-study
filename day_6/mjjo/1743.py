cedf15e74082d3f9ed624647e7d74c0ed9383ed2
jomujin <jomujin@gmail.com>
2022-11-02 22:25:26 +0900

    1743 수정

--- day_6/mjjo/1743.py
+++ day_6/mjjo/1743.py
@@ -11,10 +11,6 @@ dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 max_cnt = 0
 
-for loc in trash_loc:
-    x, y = loc
-    board[x-1][y-1] = 1
-
 def bfs(x, y):
     cnt = 1
     _deque = deque()
@@ -25,15 +21,19 @@ def bfs(x, y):
         x, y = _deque.popleft()
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            if (0<=nx<N) and (0<=ny<M) and (board[nx][ny] == 1) and (visited[nx][ny]):
+            if (0<=nx<N) and (0<=ny<M) and (board[nx][ny]) and (visited[nx][ny]):
                 visited[nx][ny] = False
                 _deque.append([nx, ny])
                 cnt += 1
     return cnt
 
+for loc in trash_loc:
+    x, y = loc
+    board[x-1][y-1] = 1
+
 for x in range(N):
     for y in range(M):
-        if (board[x][y] == 1) and (visited[x][y]):
+        if (board[x][y]) and (visited[x][y]):
             cnt = bfs(x, y)
             max_cnt = max(max_cnt, cnt)
 
