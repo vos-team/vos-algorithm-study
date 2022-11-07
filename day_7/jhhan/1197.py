3eb3d89b7cbf0fc12eec6f8612a46df9b1fa7ace
snowman95 <hun2ya9@gmail.com>
2022-11-07 22:27:17 +0900

    MST - 1197 최소 스패닝 트리

---
+++ day_7/jhhan/1197.py
@@ -0,0 +1,37 @@
+import sys
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, sys.stdin.readline().split())
+edges = []
+parent = [0] * (n+1)
+total_cost = 0
+
+for i in range(1, n+1):
+    parent[i] = i
+
+for _ in range(m):
+    a, b, cost = map(int, sys.stdin.readline().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for i in range(m):
+    cost, a, b = edges[i]
+    if find_parent(parent, a) != find_parent(parent, b):
+        union(parent, a, b)
+        total_cost += cost
+
+print(total_cost)
